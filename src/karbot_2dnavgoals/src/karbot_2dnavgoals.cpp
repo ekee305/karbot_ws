a2069e1ba3ed565b515f3b0445cc82f50c0ca5a6
RobotChicken97 <36538160+RobotChicken97@users.noreply.github.com>
2021-06-08 08:47:26 -0700

    Update to navgoals

--- src/karbot_2dnavgoals/src/karbot_2dnavgoals.cpp
+++ src/karbot_2dnavgoals/src/karbot_2dnavgoals.cpp
@@ -12,19 +12,25 @@
 #include "std_msgs/String.h"
 #include "jsoncpp.cpp"
 #include <map>
+#include <time.h>
 
 using namespace std;
 
 geometry_msgs::Point position;
 double roll, pitch, yaw;
 bool first_pose_loaded = false;
+bool first_message_received = false;
 
 std::deque<geometry_msgs::PoseStamped> high_priority_goals;
 std::deque<geometry_msgs::PoseStamped> low_priority_goals;
 std::deque<string> high_priority_job_ids;
 std::deque<string> low_priority_job_ids;
+std::deque<string> completed_job_ids;
 
 map<string, geometry_msgs::PoseStamped> locations;
+geometry_msgs::PoseStamped Goal;
+std::string current_job_id;
+std_msgs::String job_update;
 
 geometry_msgs::PoseStamped ward_a;
 geometry_msgs::PoseStamped ward_b;
@@ -53,50 +59,63 @@ void jobRequestCallback(const std_msgs::String::ConstPtr &msg) //might be quicke
 
   if (parseSuccess)
   {
+    first_message_received = true;
     const Json::Value numJobRequestsRoot = root["numJobRequests"];
     const Json::Value jobRequestsRoot = root["jobRequests"];
     int numJobReqs = numJobRequestsRoot.asInt();
     cout << "Number of job requests is " << numJobReqs << "\n";
-    cout << "Size of high queue " << high_priority_job_ids.size()<< "\n";
-    cout << "Size of low queue " << low_priority_job_ids.size()<< "\n";
+    // cout << "Size of high queue " << high_priority_job_ids.size() << "\n";
+    // cout << "Size of low queue " << low_priority_job_ids.size() << "\n";
     for (int index = 0; index <= numJobReqs - 1; index++)
     {
 
       auto it_high = std::find(high_priority_job_ids.begin(), high_priority_job_ids.end(), jobRequestsRoot[index]["job_id"].asString());
       auto it_low = std::find(low_priority_job_ids.begin(), low_priority_job_ids.end(), jobRequestsRoot[index]["job_id"].asString());
-      if (it_high != high_priority_job_ids.end())
-      {
-        int distance = it_high - high_priority_job_ids.begin();
-        cout << "Distance high " << distance << "\n";
-      }
-      else
-      {
-        if (jobRequestsRoot[index]["priority"].asString() == "High Priority")
-        {
-          auto high_it = locations.find(jobRequestsRoot[index]["source"].asString());
-          // cout << "High Priority Job is " << it->first << " = " << it->second << endl;
-          high_priority_goals.push_back(high_it->second);
-          high_priority_job_ids.push_back(jobRequestsRoot[index]["job_id"].asString());
-        }
-      }
+      auto it_completed = std::find(completed_job_ids.begin(), completed_job_ids.end(), jobRequestsRoot[index]["job_id"].asString());
 
-      if (it_low != low_priority_job_ids.end())
+      // Check if already completed
+      if (it_completed != completed_job_ids.end())
       {
-        int distance = it_low - low_priority_job_ids.begin();
-        cout << "Distance low " << distance << "\n";
+        int distance = it_completed - completed_job_ids.begin();
+        // cout << "Number of Completed Jobs " << completed_job_ids.size() << "\n";
       }
       else
       {
-        if (jobRequestsRoot[index]["priority"].asString() == "Low Priority")
+
+        if (it_high != high_priority_job_ids.end())
         {
-          auto low_it = locations.find(jobRequestsRoot[index]["source"].asString());
-          low_priority_goals.push_back(low_it->second);
-          low_priority_job_ids.push_back(jobRequestsRoot[index]["job_id"].asString());
+          int distance = it_high - high_priority_job_ids.begin();
+          // cout << "Distance high " << distance << "\n";
+        }
+        else
+        {
+          if (jobRequestsRoot[index]["priority"].asString() == "High Priority")
+          {
+            auto high_it = locations.find(jobRequestsRoot[index]["source"].asString());
+            // cout << "High Priority Job is " << it->first << " = " << it->second << endl;
+            high_priority_goals.push_back(high_it->second);
+            high_priority_job_ids.push_back(jobRequestsRoot[index]["job_id"].asString());
+          }
+        }
+
+        if (it_low != low_priority_job_ids.end())
+        {
+          int distance = it_low - low_priority_job_ids.begin();
+          // cout << "Distance low " << distance << "\n";
+        }
+        else
+        {
+          if (jobRequestsRoot[index]["priority"].asString() == "Low Priority")
+          {
+            auto low_it = locations.find(jobRequestsRoot[index]["source"].asString());
+            low_priority_goals.push_back(low_it->second);
+            low_priority_job_ids.push_back(jobRequestsRoot[index]["job_id"].asString());
+          }
         }
       }
     }
-
-    // cout << "Result is " << resultValue[1]["priority"]<< "\n";
+    //  cout << job_update.data << endl;
+    //  jobresp_pub.publish(job_update);
   }
 }
 
@@ -123,35 +142,91 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::NodeHandle a;
   ros::NodeHandle jobReqHandle;
+
+  ros::NodeHandle jobRespHandle;
+  ros::Publisher jobresp_pub = jobRespHandle.advertise<std_msgs::String>("/karbot/jobResponse", 0);
+
   ros::Publisher goal_pub = n.advertise<geometry_msgs::PoseStamped>("/goal", 1000);
   ros::Subscriber amcl_sub = a.subscribe("/amcl_pose", 1, amclCallback);
   ros::Subscriber jobreq_sub = jobReqHandle.subscribe("/karbot/jobRequest", 1000, jobRequestCallback);
+
   ros::Rate loop_rate(5);
+  ros::Rate pub_rate(0.2);
+
   int index = 0;
   //Wait for AMCL node to publish pose before proceeding
-  while (!first_pose_loaded)
+  // while (!first_pose_loaded)
+  // {
+  //   ROS_WARN_ONCE("Waiting for AMCL");
+  //   ros::spinOnce();
+  // }
+  ROS_WARN_ONCE("AMCL received");
+
+  while (!first_message_received)
   {
-    ROS_WARN_ONCE("Waiting for AMCL");
+    ROS_WARN_ONCE("Waiting for comms with web interface");
     ros::spinOnce();
   }
-  ROS_WARN_ONCE("AMCL received");
-  std::deque<geometry_msgs::PoseStamped> goals;
-  geometry_msgs::PoseStamped temp_goal;
-  double goal_array[] = {12, 22, 27.3, 35.1, 27.1, 21.2, 12, 15};
-  temp_goal.header.frame_id = "map";
-  for (int i = 0; i < 8; i = i + 2)
-  {
-    temp_goal.pose.position.x = goal_array[i];
-    temp_goal.pose.position.y = goal_array[i + 1];
-    goals.push_back(temp_goal);
-  }
-  //Loop to publish goals in order and in a loop
+
   while (ros::ok())
   {
-    goal_pub.publish(goals[index]);
-    if ((get_dist(goals[index].pose.position, position)) < 0.6)
+
+    if (high_priority_job_ids.size() != 0)
     {
-      index = ((index + 1) % 4);
+      // Set goal point to top of queue
+      Goal = high_priority_goals.front();
+      if (current_job_id != high_priority_job_ids.front())
+      {
+        current_job_id = high_priority_job_ids.front();
+        // Send job update
+        job_update.data = "{\"job_id\": " + current_job_id + ", " + "\"status\":\"In Progress\"}";
+        cout << job_update.data << endl;
+        jobresp_pub.publish(job_update);
+      }
+    }
+    else if (low_priority_job_ids.size() != 0)
+    {
+      // Set goal point to top of queue
+      Goal = low_priority_goals.front();
+
+      if (current_job_id != low_priority_job_ids.front())
+      {
+        current_job_id = low_priority_job_ids.front();
+        // Send job update
+        job_update.data = "{\"job_id\": " + current_job_id + ", " + "\"status\":\"In Progress\"}";
+        cout << job_update.data << endl;
+        jobresp_pub.publish(job_update);
+      }
+    }
+    else
+    {
+      // Set goal point to home
+    }
+
+    goal_pub.publish(Goal);
+
+    if ((get_dist(Goal.pose.position, position)) < 0.6)
+    {
+      // Check if its high priority job or low priority job
+      // if its a high priority job?
+      auto it_high = std::find(high_priority_job_ids.begin(), high_priority_job_ids.end(), current_job_id);
+      if (it_high != high_priority_job_ids.end())
+      {
+        high_priority_job_ids.pop_front();
+        high_priority_goals.pop_front();
+        // Send job update
+      }
+
+      // if its a low priority job?
+      auto it_low = std::find(high_priority_job_ids.begin(), high_priority_job_ids.end(), current_job_id);
+      if (it_low != low_priority_job_ids.end())
+      {
+        low_priority_job_ids.pop_front();
+        low_priority_goals.pop_front();
+        // Send job update
+      }
+
+      // ELSE ITS AT HOME?
     }
 
     ros::spinOnce();
